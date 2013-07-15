91d839ef6036587b7ee30efee77036a7ad113725
phelixnmc <githubnmc@blockchained.com>
2013-07-15 11:02:33 +0200

    Fix OpenSSL Perl

--- easywinbuilder/set_vars.bat
+++ easywinbuilder/set_vars.bat
@@ -15,5 +15,5 @@
 @set QTDOWNLOADPATH=http://download.qt-project.org/official_releases/qt/4.8/4.8.5/qt-win-opensource-4.8.5-mingw.exe
 @rem Qt5 will need changes in gather_dlls.bat
 
-@set MSYS=C:\MinGW\msys\1.0\bin
-@set PERL=%MSYS%\perl.exe
+@set MSYS=C:/MinGW/msys/1.0/bin
+@set PERL=%MSYS%/perl.exe
